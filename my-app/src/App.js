f82ab9a42df4f9e194ce7cbe6cd49924dc2df0d6
okohchiedu <okohthomas35@gmail.com>
2023-03-26 10:53:05 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+ 
+import UncontrolledExample from './Component/Carousel.jsx';
+import Home from './Component/Home.jsx'
+import BasicExample from './Component/Navbar';
+
+
+function App() {
+  return (
+    <div className='App'>
+    <h1>Welcome to react world</h1> 
+    <Home></Home>
+    <BasicExample></BasicExample>
+    <UncontrolledExample/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Component/Home.jsx
@@ -0,0 +1,25 @@
+
+
+const Home =() => {
+let firstName = "chiedu"
+let lastName = "okoh"
+
+    return(
+        <div style={{backgroundColor : 'blue',padding : 50}}>
+
+            <h3>hello from functional component</h3>
+            <h4>{firstName+ " " + lastName}</h4>
+ 
+        </div>
+    )
+}
+
+// class Home extends {Component} 
+
+// render() => {
+
+// }
+
+
+
+export default Home
